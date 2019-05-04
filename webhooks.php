0dfc2f10fd185231aeb6de0a46ed575be7e388ed
PittarCAn <50260597+PittarCAn@users.noreply.github.com>
2019-05-04 16:05:09 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '3U8Fq8DzK4/yM0D/eCM4FE3Ba79Ecr2Qoe0vljz8ag+N6PLyrY5k4mvt1FzXMwIh4uxZ280NK0UDIsNEqLv4l1XhKo1af3oGe1F42Pi54C7KXWa6nBvGpw0R5I7kdt2JjqWinuRZcOZlZXy3gKwSswdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
